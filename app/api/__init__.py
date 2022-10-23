44fec5a68983c5e9bbe78b958623d0cd489027a2
Aahan Patel <aahan.patel@warriorlife.net>
2022-10-23 01:23:12 -0700

    Updated everything

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,24 +1,40 @@
-from flask import render_template, redirect, request, Blueprint, session, url_for
+import ska as ska
+from flask import render_template, redirect, request, Blueprint, session, url_for, make_response
 from flask_login import current_user
 from app.db import Block, db, User
 import json
+
+SECRET_KEY = "JSD87FS890sd889dfik99FDFD"
 
 # Registers flask blueprint variable for page rendering
 api_blueprint = Blueprint('api', __name__)
 
 
-@api_blueprint.route('/blocked')
+@api_blueprint.route('/blocked', methods=["GET", "POST"])
 def block():
     if request.method == "POST":
-        data = json.loads(request.data)
+        data = request.get_json()
+        print(data)
         block_url = Block(url=data["blocked_url"], block_id=data["block_id"])
         db.session.add(block_url)
         db.session.commit()
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
-@api_blueprint.route('/login')
+
+@api_blueprint.route('/extension/login', methods=["GET", "POST"])
 def login():
-    if request.method == "POST":
-        data = json.loads(request.data)
-        user = db.session.query(User).filter(User.email == data["email"]).filter(User)
-        db.session.add(block_url)
-        db.session.commit()
+    if request.method == "GET":
+        if current_user.is_authenticated:
+            blocked_urls = []
+            for i in db.session.query(Block).filter(Block.block_id==current_user.block_id).all():
+                blocked_urls.append(i.url)
+            print({"registered": True, "email": current_user.email, "block_id": current_user.block_id, "blocked_urls": blocked_urls})
+            return {"registered": True, "email": current_user.email, "block_id": current_user.block_id, "blocked_urls": blocked_urls}
+        else:
+            url = 'https://127.0.0.1:5000/login'
+            signed_url = ska.sign_url(
+                auth_user='user',
+                secret_key=SECRET_KEY,
+                url=url
+            )
+            return {"registered": False, "signed_url": signed_url}
--- routes/services.py
+++ routes/services.py
@@ -12,6 +12,14 @@ from PIL import Image
 from io import BytesIO
 import base64
 import re
+from PIL import Image
+from io import BytesIO
+import base64
+import re
+import face_recognition
+import cv2
+import numpy as np
+import time
 
 service_blueprint = Blueprint('service', __name__, template_folder='templates')
 
@@ -49,3 +57,35 @@ def verify():
         return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
     return render_template('verify.html')
 
+
+@service_blueprint.route('/cam/confirm/<string:block_id>', methods=["POST", "GET"])
+@login_required
+def confirm(block_id):
+    if request.method == "POST":
+        image_data = re.sub('^data:image/.+;base64,', '', request.values.get("imgBase64"))
+        im = Image.open(BytesIO(base64.b64decode(image_data)))
+        image_code = db_code(10)
+        img = np.array(im)
+        im.save("app/video/images/test.png")
+
+        init_img = face_recognition.load_image_file(f"app/video/images/{current_user.block_id}.jpg")
+        first_face = face_recognition.face_encodings(init_img)[0]
+
+        last_img = face_recognition.load_image_file("app/video/images/test.png")
+        last_face = face_recognition.face_encodings(init_img)[0]
+
+        known_faces = [first_face]
+        face_locations = face_recognition.face_locations(init_img)[0]
+        name = None
+        matches = face_recognition.compare_faces(known_faces, last_face)
+        if True in matches:
+            print("true detected")
+            name = current_user.name
+
+        top, right, bottom, left = 0, 0, 0, 0
+        if name != None:
+            print('hi')
+            location = face_locations
+            top, right, bottom, left = location[0] * 4, location[1] * 4, location[2] * 4, location[3] * 4
+            return redirect(url_for("service.dashboard"))
+    return render_template('confirm.html')
