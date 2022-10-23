1100fe60990e78666263ef2c2c6963d27d430165
Aahan Patel <aahan.patel@warriorlife.net>
2022-10-23 04:06:01 -0700

    Fixed Plugin

--- background.js
+++ background.js
@@ -7,25 +7,26 @@ const signInWithPopup = () => {
   })
 };*/
 
-const blockedLinks = chrome.storage.sync.get("blocked_urls",function(result){
-    var blockedLinks = result.blocked_urls
+var blockedLinks = []; 
+chrome.storage.sync.get("blocked_urls",function(result){
+    blockedLinks = result.blocked_urls
     console.log(blockedLinks)
-    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) { 
-        if (changeInfo.status == 'loading') { // when the page is loading (you can do info.status === 'complete' but you will see the page for a second or two)
-            for (var i = 0; i<blockedLinks.length; i++){
-                if (tab.url === blockedLinks[i]) {
-                    chrome.tabs.query({ // change the tab url
-                        currentWindow: true,
-                        active: true
-                    }, function (tab) {
-                        chrome.storage.sync.get("block_id",function(result){
-                            chrome.tabs.update(tab.id, {
-                                url: 'https://127.0.0.1:5000/cam/confirm/' + result.block_id
-                            });
+})
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) { // listener for tab opens
+    if (changeInfo.status == 'loading') { // when the page is loading (you can do info.status === 'complete' but you will see the page for a second or two)
+        for (var i = 0; i<blockedLinks.length; i++){
+            if (tab.url === blockedLinks[i]) {
+                chrome.tabs.query({ // change the tab url
+                    currentWindow: true,
+                    active: true
+                }, function (tab) {
+                    chrome.storage.sync.get("block_id",function(result){
+                        chrome.tabs.update(tab.id, {
+                            url: 'http://127.0.0.1:5000//cam/confirm/' + result.block_id
                         });
                     });
-                }
+            });
             }
         }
-    })
-});
+    }
+})
--- popup.js
+++ popup.js
@@ -1,7 +1,3 @@
-var block_id;
-
-
-
 let btn = document.createElement("button");
 btn.innerHTML = "Secure Tab";
 btn.addEventListener("click", function() {
@@ -9,51 +5,57 @@ btn.addEventListener("click", function() {
     chrome.tabs.query(
         {active:true},
         tabs=>{
-            const tab=tabs[0];
-            var current_tab = tab.url;
+            chrome.storage.sync.get("block_id",function(result){
+                const tab=tabs[0];
+                var current_tab = tab.url;
 
-            var xhttp = new XMLHttpRequest();
-            xhttp.open("POST", "http://127.0.0.1:5000/blocked", true);
-            xhttp.setRequestHeader('Content-Type', 'application/json');
-            var tab_json = JSON.stringify({"block_id": block_id, "blocked_url": current_tab})
-            var block_json = tab_json
-            xhttp.send(block_json)
-        
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                console.log(this.responseText);
+                var xhttp = new XMLHttpRequest();
+                xhttp.open("POST", "http://127.0.0.1:5000/blocked", true);
+                xhttp.setRequestHeader('Content-Type', 'application/json');
+                var tab_json = JSON.stringify({"block_id": result.block_id, "blocked_url": current_tab})
+                var block_json = tab_json
+                xhttp.send(block_json)
+                
+                xhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        chrome.storage.sync.get("blocked_urls",function(result){
+                            var dict = result.blocked_urls.push(tab)
+                            console.log(dict)
+                            chrome.storage.sync.set(dict)
+                        })
+                    }
                 }
-            }
+            });
         }
     )
 });
 
 chrome.storage.sync.get("block_id",function(result){
-    if (typeof result.block_id == "undefined"){ 
+    if (typeof result.key == "undefined"){ 
         var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://127.0.0.1:5000/extension/login');
+        xhr.send();
         xhr.onreadystatechange = function() {
             if (xhr.readyState == XMLHttpRequest.DONE) {
                 var response_json = JSON.parse(xhr.responseText);
+                console.log(response_json)
                 if (response_json["registered"] == false) {
                     var url = response_json["url"]
                     chrome.tabs.create({ url: url });
 
                 } else {
+                    console.log(response_json["blocked_urls"])
                     var dict = {"email": response_json["email"], "block_id": response_json["block_id"], "blocked_urls": response_json["blocked_urls"]}
                     console.log(dict)
-                    block_id = response_json["block_id"]
                     chrome.storage.sync.set(dict)
                     document.body.appendChild(btn);
                 }
             }
         }
-        xhr.open('GET', 'http://127.0.0.1:5000/extension/login', true);
-        xhr.send(null);
     } else {
-        console.log("test")
         document.body.appendChild(btn);
     }
-  });
+});
 
 
 /*
